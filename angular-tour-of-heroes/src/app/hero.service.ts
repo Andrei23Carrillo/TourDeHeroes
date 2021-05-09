6180ee20ff3a237771399889fc97931f9e51575e
Andrei23Carrillo <L163905602@chetumal.tecnm.mx>
2021-05-09 17:52:40 -0500

    commit

---
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Heroe } from './heroe';
+import { HEROES } from './mock-heroes';
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+}
